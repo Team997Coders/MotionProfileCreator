757d3bfa4fcffa3af65644b9aad31b98232a188a
BuildTools <unconfigured@null.spigotmc.org>
2018-10-15 22:38:32 -0700

    Starting to create Library for application use

---
+++ SMPC/Path.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SMPC {
+    public class Path {
+
+        private List<Segment> segments = new List<Segment>();
+
+        private double maxV, accel;
+
+        public Path(double maxV, double accel) {
+            this.maxV = maxV;
+            this.accel = accel;
+        }
+
+        public void AddStraight(double deltaPos) {
+
+        }
+
+        public void AddCurveXLock(double deltaTheta, double deltaX) {
+            double deltaY = deltaX * Math.Cos(ToRadians(deltaTheta));
+            double radius = deltaY / Math.Sin(ToRadians(deltaTheta));
+        }
+
+        public void AddCurveYLock(double deltaTheta, double deltaY) {
+
+        }
+
+        public double ToRadians(double theta) {
+            return (Math.PI / 180) * theta;
+        }
+
+    }
+}
---
+++ SMPC/Segment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMPC {
+    public class Segment {
+
+        public SegmentType segmentType;
+
+        private double leftV, rightV;
+
+        public Segment(SegmentType segmentType) {
+            this.segmentType = segmentType;
+        }
+
+        public string getInstruction() { return leftV + "," + rightV; }
+
+        // Type indentification
+
+        public class SegmentType {
+
+            public static SegmentType Curve = new SegmentType(1),
+                                        Straight = new SegmentType(0);
+
+            public int type;
+
+            public SegmentType(int type) {
+                this.type = type;
+            }
+
+        }
+
+    }
+}
